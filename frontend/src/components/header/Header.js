c570a4d567636d3c9996c845e4c37f3b709fb888
rahulpandey102 <rahulpandey9103@gmail.com>
2021-10-09 11:25:44 +0530

    first commit

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../images/logo.jpg'
+import Logo from '../../images/logo.png'
 import Search from './Search';
 import Menu from './Menu';
 
