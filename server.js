50681b38b4347848385cbb94c6624068370b139b
rahulpandey102 <rahulpandey9103@gmail.com>
2021-10-09 07:45:10 +0530

    make it better

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose.connect(URI, {
 if(process.env.NODE_ENV === 'producation'){
     app.use(express.static('frontend/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
