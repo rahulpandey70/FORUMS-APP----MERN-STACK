65b2ff97490a697450872a14571eaf259d1477d4
rahulpandey70 <rahulpandey9103@gmail.com>
2022-01-10 17:15:11 +0530

    modified globalTypes.js file

--- frontend/src/redux/actions/globalTypes.js
+++ frontend/src/redux/actions/globalTypes.js
@@ -1,21 +1,18 @@
 export const GLOBALTYPES = {
-    AUTH: "AUTH",
-    ALERT: "ALERT",
-    THEME: "THEME",
-    STATUS: "STATUS",
-    MODAL: "MODAL",
-    SOCKET: 'SOCKET',
-    PEER: 'PEER'
-}
-
+  AUTH: "AUTH",
+  ALERT: "ALERT",
+  STATUS: "STATUS",
+  MODAL: "MODAL",
+  SOCKET: "SOCKET",
+  PEER: "PEER",
+};
 
 export const EditData = (data, id, post) => {
-    const newData = data.map(item => (item._id === id ? post : item))
-    return newData;
-}
+  const newData = data.map((item) => (item._id === id ? post : item));
+  return newData;
+};
 
 export const DeleteData = (data, id) => {
-    const newData = data.filter(item => item._id !== id)
-    return newData;
-}
-
+  const newData = data.filter((item) => item._id !== id);
+  return newData;
+};
