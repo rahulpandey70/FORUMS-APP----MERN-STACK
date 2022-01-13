b8cc9570e837ae74e2a04f7613c190972197a11a
rahulpandey70 <rahulpandey9103@gmail.com>
2022-01-13 15:38:07 +0530

    modifies card footer file

--- frontend/src/components/home/post_card/CardFooter.js
+++ frontend/src/components/home/post_card/CardFooter.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Share from "../../../images/share.svg";
 import LikeButton from "../../LikeButton";
 import { useSelector, useDispatch } from "react-redux";
