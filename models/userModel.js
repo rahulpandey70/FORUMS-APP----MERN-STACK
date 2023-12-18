50d81db9b94f712468673c485f5db7361c2df3b1
rahulpandey70 <rahulpandey9103@gmail.com>
2023-12-18 14:53:52 +0530

    formatted

--- models/userModel.js
+++ models/userModel.js
@@ -1,49 +1,56 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 30
-    },
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 30,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
-    },
-    about: {
-        type: String,
-        default: "",
-        maxlength: 300
-    },
-    followers: [{
-        type: mongoose.Types.ObjectId,
-        ref: 'user'
-    }],
-    following: [{
-        type: mongoose.Types.ObjectId,
-        ref: 'user'
-    }]
+const userSchema = new mongoose.Schema(
+	{
+		fullname: {
+			type: String,
+			required: true,
+			trim: true,
+			maxlength: 30,
+		},
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+			maxlength: 30,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		avatar: {
+			type: String,
+			default:
+				"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+		},
+		about: {
+			type: String,
+			default: "",
+			maxlength: 300,
+		},
+		followers: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: "user",
+			},
+		],
+		following: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: "user",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
-}, {
-    timestamps: true
-});
-
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
