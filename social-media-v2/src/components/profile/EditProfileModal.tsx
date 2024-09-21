b2154baeb1ef377ad89c23fb760f71bf1a13136f
rahulpandey70 <rahulpandey9103@gmail.com>
2024-09-21 23:33:49 +0530

    feat: added breadcrumb

---
+++ social-media-v2/src/components/profile/EditProfileModal.tsx
@@ -0,0 +1,66 @@
+import { AtSign, Mail, User, X } from "lucide-react";
+import { Button, InputBox } from "../common";
+
+export const EditProfileModal = ({
+	setShowEditModal,
+}: {
+	setShowEditModal: any;
+}) => {
+	return (
+		<>
+			{/* Backdrop */}
+			<div className="fixed inset-0 bg-opacity-50 backdrop-blur-sm z-10" />
+			<div className="z-20 top-0 right-0 md:right-28 p-2 absolute w-full md:w-[68%] flex flex-col gap-4 border border:border rounded-md bg-background transition duration-300">
+				<div className="flex items-center justify-between">
+					<h1 className="text-lg font-semibold text-muted-foreground">
+						Edit Profile
+					</h1>
+					<X
+						size={24}
+						className="border border:border p-1 rounded-full text-muted-foreground hover:text-primary cursor-pointer"
+						onClick={() => setShowEditModal((curr: any) => !curr)}
+					/>
+				</div>
+				<div className="flex items-center justify-center gap-2">
+					<div className="w-32 h-32 border border:border rounded-full"></div>
+					<div className="flex flex-col gap-2">
+						<Button variant={"outline"}>Change Profile</Button>
+						<Button variant={"outline"}>Remove Profile</Button>
+					</div>
+				</div>
+				<div className="w-full">
+					<InputBox
+						type="text"
+						placeholder="Name"
+						name="Name"
+						disable={false}
+						id="name"
+						value="Rahul Pandey"
+						icon=<User />
+					/>
+					<InputBox
+						type="username"
+						placeholder="Username"
+						name="Username"
+						disable={false}
+						id="username"
+						value="yourusername"
+						icon=<AtSign />
+					/>
+					<InputBox
+						type="email"
+						placeholder="Email"
+						name="Email"
+						disable={false}
+						id="email"
+						value="yourgmail@something.com"
+						icon=<Mail />
+					/>
+					<div className="flex items-center justify-center">
+						<Button variant={"outline"}>Update Profile</Button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
---
+++ social-media-v2/src/components/profile/RIghtSideProfile.tsx
@@ -0,0 +1,35 @@
+export const RightSideProfile = () => {
+	return (
+		<div className="flex w-full p-4">
+			<div className="w-full sticky top-16">
+				<div className="flex flex-row gap-2 items-center justify-center p-4 shadow">
+					<div className="w-28 h-28 md:w-32 md:h-32 border border:border rounded-full"></div>
+					<div className="flex flex-col items-center md:ml-4 gap-2">
+						<h1 className="text-primary">Rahul Pandey</h1>
+						<h4 className="text-muted-foreground">@rhaulpandey</h4>
+						<h4 className="text-muted-foreground">rahul@gmail.com</h4>
+					</div>
+				</div>
+
+				<div className="flex items-center justify-center gap-4 p-2">
+					<div className="flex items-center justify-center">
+						<h1 className="text-lg text-muted-foreground font-medium leading-tighter">
+							Followings
+						</h1>
+						<span className="ml-2 text-sm">23</span>
+					</div>
+
+					<hr className="flex w-[0.2rem] h-[0.2rem] items-center justify-center text-border bg-border rounded-full" />
+
+					<div className="flex items-center justify-center">
+						<h1 className="text-lg text-muted-foreground font-medium leading-tighter">
+							Followers
+						</h1>
+						<span className="ml-2 text-sm">99</span>
+					</div>
+				</div>
+				<hr className="w-full" />
+			</div>
+		</div>
+	);
+};
