892e0e13c42de47bbe79d6610172a4ea6ee3d536
rahulpandey70 <rahulpandey9103@gmail.com>
2024-09-24 23:29:17 +0530

    added prisma and user schema

--- social-media-v2/src/lib/utils.ts
+++ social-media-v2/src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const registerValidation = z.object({
+	name: z.string(),
+	username: z.string(),
+	email: z.string().email("Invalid email format"),
+	password: z
+		.string()
+		.min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+export const loginValidation = z.object({
+	email: z.string().email("Invalid email format"),
+	password: z.string(),
+});
